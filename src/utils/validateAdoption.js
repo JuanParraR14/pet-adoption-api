6fe1ef18a05a5560a1e108155a9aeab10fac27cd
JuanParra14 <juanparrar14@gmail.com>
2026-07-13 17:26:04 -0500

    Se hacen las pruebas correspondientes y se corrigen algunos errores para que todo salga exitoso

--- src/utils/validateAdoption.js
+++ src/utils/validateAdoption.js
@@ -1,5 +1,5 @@
 export const validateAdoption = (data) => {
-   const { petName, species, adopterName } = data;
+   const { petName, species, adopterName, email } = data;
 
    if (!petName || petName.trim() === "") {
       return "Pet name is required";
@@ -11,6 +11,10 @@ export const validateAdoption = (data) => {
 
    if (!adopterName || adopterName.trim() === "") {
       return "Adopter name is required";
+   }
+
+   if (!email.includes("@")) {
+      return "invalid email"
    }
 
    return null;
